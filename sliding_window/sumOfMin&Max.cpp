e9b1b3e9046547137fc4841ac33efad481e69973
Neel <neelakantanaiduchittela@gmail.com>
2024-08-08 23:52:47 +0530

    sliding_window method

--- sliding_window/sumOfMin&Max.cpp
+++ sliding_window/sumOfMin&Max.cpp
@@ -9,19 +9,33 @@ int main(){
     int a[n];
     for (int i = 0; i < n; i++)
         cin >> a[i];
-    multiset<pair<int, int>> st;
+    deque<int> s(k), g(k);
     int sum = 0;
-    int i = 0, j = 0;
-    while(i<n && j<n){
-        st.insert({a[j], j});
-        if(j-i+1 == k){
-            int mini = st.begin()->first;
-            int maxi = st.rbegin()->first;
-            sum += (mini + maxi);
-            st.erase({a[i], i});
-            i++;
-        }
-        j++;
+    int i = 0;
+    for (; i < k;i++){
+        while(!s.empty() && a[s.back()] >= a[i])
+            s.pop_back();
+        while(!g.empty() && a[g.back()] <= a[i])
+            g.pop_back();
+        s.push_back(i);
+        g.push_back(i);
     }
+
+    for (; i < n;i++){
+        sum += (a[s.front()] + a[g.front()]);
+        while(!s.empty() && s.front() <= i-k)
+            s.pop_front();
+        while(!g.empty() && g.front() <= i-k)
+            g.pop_front();
+        
+        while (!s.empty() && a[s.back()] >= a[i])
+            s.pop_back();
+        while (!g.empty() && a[g.back()] <= a[i])
+            g.pop_back();
+        s.push_back(i);
+        g.push_back(i);
+    }
+
+    sum += (a[s.front()] + a[g.front()]);
     cout << sum << endl;
 }
