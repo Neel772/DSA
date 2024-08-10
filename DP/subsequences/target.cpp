d9ead5f7a37a113e8efaaf78023da65fe8dac2ff
Neelakanta <153833919+Neel772@users.noreply.github.com>
2024-08-10 13:11:53 +0530

    Update target.cpp

--- DP/subsequences/target.cpp
+++ DP/subsequences/target.cpp
@@ -10,7 +10,7 @@ bool helper(int idx, int target, vector<vector<int>> &dp, vector<int>arr){
         return dp[idx][target];
     bool not_taken = helper(idx - 1, target, dp, arr);
     bool take = false;
-    if(arr[idx] < target){
+    if(arr[idx] <= target){
         take = helper(idx - 1, target - arr[idx], dp, arr);
     }
     return dp[idx][target] =  take || not_taken;
